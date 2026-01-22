7b546e6d64de16aa5f7666b31e19d046506fb3d2
Ethan-crh <ethanhandball@gmail.com>
2026-01-23 00:22:34 +0100

    Modification Navbar

--- Front/src/components/Navbar.js
+++ Front/src/components/Navbar.js
@@ -5,33 +5,24 @@ import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
-    <ul className="nav justify-content-center my-3">
-      <li className="nav-item">
-        <Link className="nav-link" to="/">
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/sample">
-          Sample
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/salle">
-          Salle
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/capteur">
-          Capteur
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/map">
-          Map
-        </Link>
-      </li>
-    </ul>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <a
+          href="https://episen.u-pec.fr/"   // on introduit le lien cible
+        >
+          <img src={logoEpisen} alt="Site EPISEN" className="navbar-logo" />
+        </a>
+      </div>
+
+      <div className="navbar-links"> 
+        <Link to="/">Accueil</Link>   
+        <Link to="/salle">Salles</Link>
+        <Link to="/capteur">Capteurs</Link>
+        <Link to="/map">Carte</Link>
+        <Link to="/offres">Offres</Link>
+      </div>
+    </nav>
   );
 }
 // Les liens sont gérés par le routeur (comme pour le proto)
+
