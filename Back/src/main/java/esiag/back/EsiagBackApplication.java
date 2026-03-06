51e71f9cefaa8c3135bbcc92a2bd2bea012bc903
AbderraoufRedjechta <raoufredjechta@gmail.com>
2026-03-06 02:18:34 +0100

    fix back

--- Back/src/main/java/esiag/back/EsiagBackApplication.java
+++ Back/src/main/java/esiag/back/EsiagBackApplication.java
@@ -2,9 +2,7 @@ package esiag.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class EsiagBackApplication {
 
